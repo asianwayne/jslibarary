ef5df8dab74868e628edffc45582a004e2ab5891
Wayne Shen <53993659+asianwayne@users.noreply.github.com>
2022-02-23 16:17:49 +0800

    Update jsbasic.js

--- jsbasic.js
+++ jsbasic.js
@@ -8,3 +8,26 @@ console.log(fruits.indexOf('apple')); //查询apple的顺序
 
 console.log(Array.isArray(fruits)); //来确定是不是array
 console.log(fruits);
+
+//Obejct 
+const person = {
+name:'wayne',
+gender:'male',
+age:22,
+hobies:['music','sports','basketball'],
+address:{
+street:'sanjie',
+city:'Shengzhou',
+country:'China'
+}
+}
+console.log(person.hobies[2]);
+
+const {name,age,address:{city}} = person; //pulling our the variable, 把name age 变量从 person object address object里面的city 变量 拉出来  address{ }  是 object的 拉法 
+console.log(name,city);
+
+person.email = "wayne@wayne.com";  //外部添加属性到object 
+
+console.log(person);
+
+//array of objects 

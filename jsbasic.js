55b1dad8d63c03365064106e352ff74c9b1ee2c0
Wayne Shen <53993659+asianwayne@users.noreply.github.com>
2022-03-05 14:15:01 +0800

    Update jsbasic.js

--- jsbasic.js
+++ jsbasic.js
@@ -122,7 +122,12 @@ console.log(todoComplete);
 const x = '10';
 if(x === 10) {
 console.log('x is equal to 10')
-} else {
-console.log('x is not equal 10')
+} else if (x > 10) {
+	console.log('x is greater than 10;)
+
 }
+else {
+console.log('x is less than  10')
+}
+
 
